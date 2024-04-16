65a6d1be6d0af6959a4b3b15c9a173e459a16c5c
Erik Warling <snorklecraft@gmail.com>
2024-04-16 00:03:51 -0700

    add completer tab and buttons, whoops

---
+++ src/components/tabs/completer/CompleterButton.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "CompleterButton",
+  data() {
+    return {
+      isOn: false,
+    };
+  },
+  computed: {
+    classObject() {
+      return {
+        "o-completer-btn": true,
+        "o-completer-btn--color": true,
+      };
+    },
+    text() {
+      return this.isOn ? "Turn Completer Off" : "Turn Completer On";
+    }
+  },
+  methods: {
+    update() {
+      this.isOn = player.completerOn;
+    },
+    clicked() {
+      player.completerOn = !player.completerOn;
+    }
+  }
+};
+</script>
+
+<template>
+  <button
+    v-tooltip="tooltip"
+    :class="classObject"
+    @click="clicked"
+  >
+    {{ text }}
+  </button>
+</template>
---
+++ src/components/tabs/completer/CompleterTab.vue
@@ -0,0 +1,16 @@
+<script>
+import CompleterButton from "./CompleterButton";
+
+export default {
+  name: "CompleterTab",
+  components: {
+    CompleterButton
+  }
+};
+</script>
+
+<template>
+  <div class="l-completer-tab">
+    <CompleterButton class="l-completer-tab__completer-btn" />
+  </div>
+</template>
